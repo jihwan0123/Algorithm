83c0ce8dd4d96402e06656856c76ba887ad27d39
= <qkdwlghks00@gmail.com>
2021-06-03 22:38:11 +0900

    Update README

--- SWEA/D5/swea_10202.py
+++ SWEA/D5/swea_10202.py
@@ -13,13 +13,20 @@ for tc in range(1, 1 + T):
     c = input()
 
     counter = 0
+    # n번째 자리까지 비교하면서
     for i in range(n):
+        # 같으면 교환 x
         if a[i] == b[i] == c[i]:
             continue
+        # 다른게 있으면
         else:
-            same = {a[i], b[i], c[i]}  # 같은 만큼 남는다.
+            # set으로 체크해서 중복 제거
+            same = {a[i], b[i], c[i]}
+            # 같은게 생겼다면 len = 2가 되므로 1개만 바꾸면 됨
             if len(same) == 2:
                 counter += 1
+            # 아니면 2자리 다 바꾸기
             else:
                 counter += 2
+                
     print('#{} {}'.format(tc, counter))
