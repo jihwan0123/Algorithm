badcee9b5e064c59858f3b6d6fa417eeb52844cd
= <qkdwlghks00@gmail.com>
2021-06-12 23:52:36 +0900

    0612 JUNGOL

---
+++ JUNGOL/Beginner_Coder/jo_1338.py
@@ -0,0 +1,26 @@
+# 1338. 문자삼각형1
+
+'''
+        A
+      B F
+    C G J
+  D H K M
+E I L N O
+'''
+# N = 5
+N = int(input())
+arr = [[' ' for _ in range(N)] for _ in range(N)]
+total = N*(N+1)//2
+start = ord('A')
+for i in range(N):
+    k = N
+    for j in range(i,N):
+        k -= 1
+        arr[j][k] = chr(start)
+        start += 1
+        if start > 90:
+            start = 65
+for a in arr:
+    print(' '.join(a))
+
+
