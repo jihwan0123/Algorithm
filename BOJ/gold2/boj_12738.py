65d629d665eec15310855fa60d85304f4fd1df19
jihwan0123 <qkdwlghks00@gmail.com>
2021-08-08 00:18:53 +0900

    0808 BOJ
    가장 긴 증가하는 부분수열 1 ~ 4

---
+++ BOJ/gold2/boj_12738.py
@@ -0,0 +1,21 @@
+# 12738. 가장 긴 증가하는 부분수열 3
+
+import sys
+from bisect import bisect_left
+input = sys.stdin.readline
+
+n = int(input())
+nums = list(map(int, input().split()))
+
+ans = [nums[0]]
+
+for num in nums:
+    if num > ans[-1]:
+        ans.append(num)
+    elif num == ans[-1]:
+        continue
+    else:
+        idx = bisect_left(ans, num)
+        ans[idx] = min(num, ans[idx])
+
+print(len(ans))
---
+++ BOJ/silver2/boj_11053.py
@@ -0,0 +1,20 @@
+# 11053. 가장 긴 증가하는 부분수열
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+nums = list(map(int, input().split()))
+
+dp = [0] * n
+ans = 0
+for i in range(n):  # num 전체 돌면서
+    maxV = 0  # max값 초기화
+    for j in range(i+1):  # i보다 앞의 값중에서
+        if nums[j] < nums[i]:  # 작은값이 있으면
+            maxV = max(maxV, dp[j])  # 작은값의 dp값 최댓값에 저장
+
+    dp[i] = 1 + maxV  # 최댓값 + 1이 i번째 사용해서 만들 수 있는 가장 긴 부분수열 길이
+    ans = max(ans, dp[i])  # 정답에 최댓값 갱신
+
+print(ans)
