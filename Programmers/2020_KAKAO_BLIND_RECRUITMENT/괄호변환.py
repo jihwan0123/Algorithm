4455dde080f13a34bacb01e0f42a058d8f8757b5
jihwan0123 <qkdwlghks00@gmail.com>
2021-08-27 00:26:20 +0900

    0826 PGS. 괄호변환

---
+++ Programmers/2020_KAKAO_BLIND_RECRUITMENT/괄호변환.py
@@ -0,0 +1,61 @@
+# 괄호변환
+
+# 올바른 괄호 문자열인지 체크
+def check(p):
+    stack = []
+    for i in p:
+        if i == ')':
+            if not stack:
+                return False
+            stack.pop()
+        else:
+            stack.append(i)
+    return True
+
+# u, v 나누기
+def divide(p):
+    cntR = cntL = 0
+    for i in range(len(p)):
+        if p[i] == ')':
+            cntL += 1
+        else:
+            cntR += 1
+        
+        if cntL == cntR:
+            return p[:i+1], p[i+1:]
+
+
+def solution(p):
+    answer = ''
+    # 입력이 빈 문자열인 경우 빈 문자열 반환
+    # 만약 p가 이미 "올바른 괄호 문자열"이라면 그대로 return
+    if (p == '') | check(p):
+        return p
+
+    # 2. 문자열 w를 두 "균형잡힌 괄호 문자열" u, v로 분리합니다. 
+    # 단, u는 "균형잡힌 괄호 문자열"로 더 이상 분리할 수 없어야 하며, v는 빈 문자열이 될 수 있습니다. 
+    u, v = divide(p)
+    # 3. 문자열 u가 "올바른 괄호 문자열" 이라면 문자열 v에 대해 1단계부터 다시 수행합니다. 
+    #   3-1. 수행한 결과 문자열을 u에 이어 붙인 후 반환합니다. 
+    if check(u):
+        return u + solution(v)
+    # 4. 문자열 u가 "올바른 괄호 문자열"이 아니라면 아래 과정을 수행합니다. 
+    else:
+        #   4-1. 빈 문자열에 첫 번째 문자로 '('를 붙입니다. 
+        #   4-2. 문자열 v에 대해 1단계부터 재귀적으로 수행한 결과 문자열을 이어 붙입니다. 
+        #   4-3. ')'를 다시 붙입니다. 
+        answer = '(' + solution(v) + ')'
+        #   4-4. u의 첫 번째와 마지막 문자를 제거하고, 나머지 문자열의 괄호 방향을 뒤집어서 뒤에 붙입니다. 
+        #   4-5. 생성된 문자열을 반환합니다.
+        for i in u[1:-1]:
+            if i == '(':
+                answer += ')'
+            else:
+                answer += '('
+
+    return answer
+
+
+print(solution("(()())()"))
+print(solution(")("))
+print(solution("()))((()"))
