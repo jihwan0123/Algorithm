527faad37fa1c4b32b5ef9c22f7ce7e9d9d43fb4
jihwan0123 <qkdwlghks00@gmail.com>
2021-09-29 23:43:55 +0900

    0929 BOJ 17396.백도어

---
+++ BOJ/gold5/boj_17396.py
@@ -0,0 +1,40 @@
+# 17396. 백도어
+
+import heapq
+import sys
+input = sys.stdin.readline
+INF = sys.maxsize
+
+n, m = map(int, input().split())
+nums = list(map(int, input().split()))
+nums[-1] = 0
+adj = [[] for _ in range(n)]
+
+
+def dijkstra():
+    dist = [INF] * n
+    q = []
+    heapq.heappush(q, (0, 0))
+    dist[0] = 0
+    while q:
+        w, cur = heapq.heappop(q)
+        if dist[cur] < w:
+            continue
+
+        for next, next_w in adj[cur]:
+            if dist[next] > w + next_w:
+                dist[next] = w + next_w
+                heapq.heappush(q, (dist[next], next))
+
+    return dist[-1]
+
+
+for _ in range(m):
+    a, b, t = map(int, input().split())
+    if nums[a] or nums[b]:
+        continue
+    adj[a].append([b, t])
+    adj[b].append([a, t])
+
+ans = dijkstra()
+print(ans if ans != INF else -1)
