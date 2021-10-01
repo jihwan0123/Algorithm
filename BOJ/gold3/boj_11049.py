85b0ff76fb39db04de36f09486ca2b9e21e3ebd1
jihwan0123 <qkdwlghks00@gmail.com>
2021-10-02 01:00:40 +0900

    BOJ 11049. 행렬 곱셈 순서 수정
    코드 수정 및 주석 추가

--- BOJ/gold3/boj_11049.py
+++ BOJ/gold3/boj_11049.py
@@ -11,16 +11,12 @@ for j in range(1, n):
     for i in range(n-j):
         # 2개씩 곱한거부터 n개 곱한거 순서대로 계산
         start, end = i, i+j
-        dp[start][end] = sys.maxsize
-        # start 부터 end까지 곱했을때 최솟값
-        for k in range(start, end):
-            x = min(dp[start][end],
-                    dp[start][k] + dp[k+1][end] +
-                    arr[start][0] * arr[k][1] * arr[end][1])
-            dp[start][end] = x
+        # start 부터 end까지 곱했을때 최솟값 =
+        # start ~ k 번까지의 연산 수 + k+1부터 end까지의 연산 수
+        # + start[0]번 end[1]번 k번
+        dp[start][end] = min([dp[start][k] + dp[k+1][end] + \
+                                arr[start][0] * arr[k][1] * arr[end][1] \
+                                    for k in range(start, end)])
 
-            # for d in dp:
-            #     print(*d)
-            # print()
 
 print(dp[0][-1])
