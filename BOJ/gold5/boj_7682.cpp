ddcb876e7ad31606e6b1f8800a003c06da10bab2
jihwan0123 <qkdwlghks00@gmail.com>
2021-10-22 21:24:33 +0900

    1022 BOJ 7682. 틱택토

---
+++ BOJ/gold5/boj_7682.cpp
@@ -0,0 +1,152 @@
+﻿// boj_7682.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cstring>
+#include <cstdlib>
+using namespace std;
+
+char arr[3][3];
+string input;
+int o_cnt, x_cnt;
+
+bool check_x() {
+    int chk = 0;
+    for (int i = 0; i < 3; i++) {
+        chk = 0;
+        for (int j = 0; j < 3; j++) {
+            if (arr[i][j] == 'X') {
+                chk++;
+            }
+        }
+        if (chk == 3) {
+            return true;
+        }
+    }
+    for (int i = 0; i < 3; i++) {
+        chk = 0;
+        for (int j = 0; j < 3; j++) {
+            if (arr[j][i] == 'X') {
+                chk++;
+            }
+        }
+        if (chk == 3) {
+            return true;
+        }
+    }
+
+    if (arr[1][1] == 'X') {
+        if ((arr[0][0] == 'X') && (arr[2][2] == 'X')) { // 대각
+            return true;
+        }
+        else if ((arr[0][2] == 'X') && (arr[2][0] == 'X')) { // 대각
+            return true;
+        }
+    }
+    return false;
+}
+
+bool check_o() {
+    int chk = 0;
+    for (int i = 0; i < 3; i++) {
+        chk = 0;
+        for (int j = 0; j < 3; j++) {
+            if (arr[i][j] == 'O') {
+                chk++;
+            }
+        }
+        if (chk == 3) {
+            return true;
+        }
+    }
+    for (int i = 0; i < 3; i++) {
+        chk = 0;
+        for (int j = 0; j < 3; j++) {
+            if (arr[j][i] == 'O') {
+                chk++;
+            }
+            else {
+                chk = 0;
+                break;
+            }
+        }
+        if (chk == 3) {
+            return true;
+        }
+    }
+
+    if (arr[1][1] == 'O') {
+        if ((arr[0][0] == 'O') && (arr[2][2] == 'O')) { // 대각
+            return true;
+        }
+        else if ((arr[0][2] == 'O') && (arr[2][0] == 'O')) { // 대각
+            return true;
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    while (true) {
+        cin >> input;
+        o_cnt = 0;
+        x_cnt = 0;
+        if (input == "end") {
+            break;
+        }
+        else {
+            for (int i = 0; i < 9; i++) {
+                arr[i/3][i%3] = input[i];
+                if (input[i] == 'O') {
+                    o_cnt++;
+                }
+                else if (input[i] == 'X') {
+                    x_cnt++;
+                }
+            }
+        }
+        if (!((x_cnt-o_cnt == 1) || (x_cnt - o_cnt == 0))) {
+            cout << "invalid" << "\n";
+            continue;
+        }
+
+        bool x = false;
+        bool o = false;
+        x = check_x();
+        o = check_o();
+        // 둘다 불가능한데 9개를 다 놨으면 valid
+        if ((!x && !o) && (x_cnt + o_cnt == 9)) {
+            cout << "valid" << "\n";
+            continue;
+        }
+        else {
+            // 동시에 성공 불가능
+            if ((x && o) || (!x && !o)) {
+                cout << "invalid" << "\n";
+                continue;
+            }
+            // X가 3개가 연결되었을 경우
+            // X의 수 = Y의 수 + 1 이어야한다.
+            if (x && (x_cnt != (o_cnt+1))) {
+                cout << "invalid" << "\n";
+                continue;
+            }
+
+            // O가 3개 연결되었을 경우
+            // X 수가 더 많으면 invalid
+            if (o && (x_cnt > o_cnt)) {
+                cout << "invalid" << "\n";
+                continue;
+            }
+        }
+        cout << "valid" << "\n";
+    }
+    return 0;
+}
---
+++ BOJ/silver5/boj_11931.cpp
@@ -0,0 +1,27 @@
+﻿// boj_11931.cpp
+// 수 정렬하기 4
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int n;
+vector<int> arr;
+
+int main()
+{
+    ios::sync_with_stdio(false); 
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int temp;
+        cin >> temp;
+        arr.push_back(temp);
+    }
+    sort(arr.begin(), arr.end(), greater<int>());
+    for (int a: arr) {
+        cout << a << '\n';
+    }
+}
