d7276a1860f3573d3380d56f9c0fd06a06ba8759
jihwan0123 <qkdwlghks00@gmail.com>
2021-10-31 11:31:02 +0900

    1031 BOJ 17071. 숨바꼭질5

--- BOJ/gold1/boj_17071.py
+++ BOJ/gold1/boj_17071.py
@@ -1,27 +1,40 @@
 # 17071. 숨바꼭질5
 
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+
+def bfs(n, k):
+    q = deque([n])
+    visited = [[False]*500001 for _ in range(2)]
+    visited[0][n] = True
+    cnt = 0
+    while q:
+        flag = cnt % 2  # 현재 짝수번째인지 홀수번째인지
+
+        if k > 500000:
+            return -1
+
+        if visited[flag][k]:  # 방문했으면, 현재 cnt 출력
+            return cnt
+
+        flag = ~flag  # 다음 이동할 flag는 반대로
+
+        length = len(q)
+        for _ in range(length):  # 현재 cnt번째 q에 들어있는 만큼만 반복
+            me = q.popleft()
+            for next in (me*2, me-1, me+1):  # *2, 1, -1
+                # 방문하지 않았으면
+                if 0 <= next <= 500000 and not visited[flag][next]:
+                    # 방문처리하고 추가
+                    visited[flag][next] = True
+                    q.append(next)
+        cnt += 1
+        k += cnt
+
+    return -1
+
+
 N, K = map(int, input().split())
-# N : 수빈, K : 동생
-
-def bfs(idx, a, b):
-    global ans
-    if b > 500000:
-        ans = -1
-        return
-    if a == b:
-        ans = idx
-        return
-
-    elif a > b:
-        if a % 2:
-            a -= 1
-        else:
-            a += 1
-    else:
-        a = a * 2
-    idx += 1
-    bfs(idx, a, b+idx)
-
-ans = 0
-bfs(0,N,K)
-print(ans)
+print(bfs(N, K))
