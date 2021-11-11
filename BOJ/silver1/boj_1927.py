a4b1299b0b1ac3a30a8626eb984ebde95cf8b5ee
jihwan0123 <qkdwlghks00@gmail.com>
2021-11-11 18:12:57 +0900

    BOJ 1927, 10866
    1927. 최소힙, 10866.덱

---
+++ BOJ/silver1/boj_1927.py
@@ -0,0 +1,17 @@
+# 1927. 최소 힙
+
+from heapq import heappush, heappop
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+heap = []
+for _ in range(n):
+    x = int(input())
+    if x:
+        heappush(heap, x)
+    else:
+        if heap:
+            print(heappop(heap))
+        else:
+            print(0)
---
+++ BOJ/silver4/boj_10866.py
@@ -0,0 +1,41 @@
+# 10866. 덱
+
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+q = deque()
+for _ in range(n):
+    x = input().split()
+    if x[0] == 'push_back':
+        q.append(x[1])
+    elif x[0] == 'push_front':
+        q.appendleft(x[1])
+    elif x[0] == 'pop_front':
+        if q:
+            print(q.popleft())
+        else:
+            print(-1)
+    elif x[0] == 'pop_back':
+        if q:
+            print(q.pop())
+        else:
+            print(-1)
+    elif x[0] == 'size':
+        print(len(q))
+    elif x[0] == 'empty':
+        if q:
+            print(0)
+        else:
+            print(1)
+    elif x[0] == 'front':
+        if q:
+            print(q[0])
+        else:
+            print(-1)
+    elif x[0] == 'back':
+        if q:
+            print(q[-1])
+        else:
+            print(-1)
