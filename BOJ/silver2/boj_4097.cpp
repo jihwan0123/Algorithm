0ab3de44d1aa3cb273e1a39e0b14f14541449c9d
jihwan0123 <qkdwlghks00@gmail.com>
2021-12-17 15:33:36 +0900

    BOJ 4097. 수익

---
+++ BOJ/silver2/boj_4097.cpp
@@ -0,0 +1,34 @@
+﻿// boj_4097.cpp
+// 수익
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    while (true) {
+        int n;
+        cin >> n;
+        if (n == 0) {
+            return 0;
+        }
+        int total = 0;
+        int ans = -10000;
+        while (n--) {
+            int x;
+            cin >> x;
+            total += x;
+            ans = max(ans, total);
+            if (total < 0)
+                total = 0;
+        }
+
+        cout << ans << '\n';
+    }
+    return 0;
+}
